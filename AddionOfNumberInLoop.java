f7dba9e6dde704191916437b34cdee07533354d2
Shubhtiwari29 <103238096+Shubhtiwari29@users.noreply.github.com>
2022-04-10 10:02:41 +0530

    This is my first commit with original file.

---
+++ AddionOfNumberInLoop.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class AddionOfNumberInLoop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1,n2,add=0;
+		char ans=0;
+		do
+		{
+			System.out.println("Please enter the value of n1");
+			n1=sc.nextInt();
+			System.out.println("Please enter the valur of n2");
+			n2 = sc.nextInt();
+			if (n1>=0 || n2>=0)
+			{
+				add= n1+n2;
+			}
+			System.out.println("Addition of two number is = "+add);
+			System.out.println("Do you want to continue?");
+			ans = sc.next().charAt(0);
+		}
+		while (ans == 'Y'  || ans == 'y');
+		System.out.println("Addition of previous number was = "+add);
+		
+
+	}
+
+}
